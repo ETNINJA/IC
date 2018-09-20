aca80c578c8cca7cf47f4a1dd78453f41df50b0e
ETNINJA <annealmd@gmail.com>
2018-09-20 17:00:25 -0300

    Algorithm pronto, SEM GRÁFICO.

--- Trab_SVM.py
+++ Trab_SVM.py
@@ -1,71 +1,35 @@
-#Anne Almeida, Geovane Richards e Robert
-# SVM - acurácia e matrix de confusão
-
-# bilbis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 from sklearn import datasets		# To Get iris dataset
 from sklearn import svm    			# To fit the svm classifier
 import numpy as np                  # manipular dados
 import pandas as pd
 import matplotlib.pyplot as plt     # To plotar
 
-
-# import iris dataset
 iris = datasets.load_iris()
 
-# *****************PARA ENTENDER O DATABASE
-
-# print na description
-print ("Iris data set Description :: ", iris['DESCR'])
-
-#print os dados
-print ("Iris feature data :: ", iris['data'])
-
-# Classifica 0 1 2 (setosa , versicolour e virginica)
-print ("Iris target TODOS :: ", iris['target'])
-print()
-print ("setosa", iris.target[:50])
-print()
-print ("versicolour", iris.target[50:100])
-print()
-print ("virginica", iris.target[100:150])
-
-#***************** FAZENDO O SPLIT 40 para o training and 10 for the test
-
-X = iris.data[:, :4]  # todas as features.
+# Create feature and target arrays
+X = iris.data[:,:3]
 y = iris.target
-print ("teste", y)
 
-X_trainS = iris.data[:40, 0:3]
-X_testS = iris.target[40:50]
-y_trainS = iris.data[:40, 0:3]
-y_testS = iris.target[40:50]
-print("y_test Setosa", y_testS)
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
 
-X_trainV = iris.data[50:90, 0:3]
-X_testV = iris.target[90:100]
-y_trainV = iris.data[50:90, 0:3]
-y_testV = iris.target[90:100]
-print("y_test Versicolour", y_testV)
-
-X_trainVi = iris.data[100:140, 0:3]
-X_testVi = iris.target[140:150]
-y_trainVi = iris.data[100:140, 0:3]
-y_testVi = iris.target[140:150]
-print("y_test Virginica", y_testVi)
-
-X_train = iris.data[100:140, 0:3]
-X_test = iris.target[140:150]
-y_train = iris.data[100:140, 0:3]
-y_test = np.array([y_testS, y_testV, y_testVi])
-print("y_test CONCATENADOS", y_test)
+print(X_train)
+print()
+print(X_test)
+print()
+print(y_test)
 
 
+classifier = svm.SVC(kernel='linear', C=1.0,gamma= 'auto').fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
 
+# Print the accuracy
+print("Accuracy",classifier.score(X_test, y_test))
 
-
-#***************************SVM daqui p baixo **************************
-
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+# Making the Confusion Matrix QUANTIDADE DE ERROS
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print()
+print("Confusion Matrix\n", cm)
